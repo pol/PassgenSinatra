f3184afdc82e1ffb2b3f4e8fe9266d8196f15154
Pol Llovet <pol.llovet@gmail.com>
2009-07-15 18:53:13 -0600

    Inital commit of old passgen

---
+++ passgen.rb
@@ -0,0 +1,17 @@
+class Passgen
+  
+  def generate(len = 8)
+    random_alpha_string(len)
+  end
+  
+  def generate_series(num,len = 8)
+    (1..num).to_a.map { |s| generate(len) }
+  end
+  
+  private
+  
+  def random_alpha_string(len)
+    chars = ['a'..'z','A'..'Z'].map { |s| [*s] }.flatten
+    (1..len).to_a.map { chars[rand(chars.length - 1)] }.join
+  end
+end
